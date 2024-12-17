f60e975d4aa75b26cf4232b09ff83aaa9408cfc6
Kyle <kdleonard@icloud.com>
2024-12-16 20:25:26 -0600

    Finished task
    But Coinbase is not on PythonAnywhere's Allowlist so for now, i sent a request to get it added.

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-from coinbase.rest import RESTClient
+# from coinbase.rest import RESTClient
 import json
 import os
 from dotenv import load_dotenv
 import http.client
+from twilio.rest import Client 
 
 load_dotenv()
 
-api_secret_key = os.environ.get("API_SECRET_KEY")
-api_public_key = os.environ.get("API_PUBLIC_KEY")
-org_id = os.environ.get("ORG_ID")
-base_url = os.environ.get("BASE_URL")
+API_SECRET_KEY = os.environ.get("API_SECRET_KEY")
+API_PUBLIC_KEY = os.environ.get("API_PUBLIC_KEY")
+ORG_ID = os.environ.get("ORG_ID")
+BASE_URL = os.environ.get("BASE_URL")
+TWILIO_API = os.environ.get("TWILIO_API_KEY")
+TWILIO_SID = os.environ.get("TWILIO_SID_KEY")
 
 
 def fetch_public_product_http_client(product_id):
-    conn = http.client.HTTPSConnection("api.coinbase.com")
+    conn = http.client.HTTPSConnection(BASE_URL)
     request_path = f"/api/v3/brokerage/market/products/{product_id}"
     
     headers = {
@@ -22,8 +25,32 @@ def fetch_public_product_http_client(product_id):
 
     conn.request("GET", request_path, "", headers)
     res = conn.getresponse()
-    data = res.read()
-    print(data.decode("utf-8"))
+    data = res.read().decode("utf-8")
+    dot_data = json.loads(data)
+    dot_price = round(float(dot_data["price"]), 2)
+    print(dot_price)
+    
+    buy_dot = False
+    
+    if dot_price < 8.70:
+        buy_dot = True 
+        
+    if buy_dot:
+        client = Client(TWILIO_SID, TWILIO_API)
+        message = client.messages.create(
+            body="Buy some DOT ma boi!!!",
+            from_="+18885998708",
+            to="+16302012552"
+        )
+        print(message.status)
+    else:
+        client = Client(TWILIO_SID, TWILIO_API)
+        message = client.messages.create(
+            body="HODL like vice grips!",
+            from_="+18885998708",
+            to="+16302012552"
+        )
+        print(message.status)
 
 if __name__ == "__main__":
     crypto_pair = "DOT-USD"  
