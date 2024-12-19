10b50dda3c2ff99e7bb29bafb43df675ec010f4a
Kyle <kdleonard@icloud.com>
2024-12-18 20:13:45 -0600

    Handled the connection with requests instead of http.client
    This fixes my error with the script running on PythonAnywhere.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 import json
 import os
 from dotenv import load_dotenv
-import http.client
 from twilio.rest import Client 
+import requests
 
 load_dotenv()
 
@@ -15,42 +15,34 @@ TWILIO_API = os.environ.get("TWILIO_API_KEY")
 TWILIO_SID = os.environ.get("TWILIO_SID_KEY")
 
 
+
 def fetch_public_product_http_client(product_id):
-    conn = http.client.HTTPSConnection(BASE_URL)
-    request_path = f"/api/v3/brokerage/market/products/{product_id}"
+    url = f"https://{BASE_URL}/api/v3/brokerage/market/products/{product_id}"
     
     headers = {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0"
     }
 
-    conn.request("GET", request_path, "", headers)
-    res = conn.getresponse()
-    data = res.read().decode("utf-8")
-    dot_data = json.loads(data)
-    dot_price = round(float(dot_data["price"]), 2)
-    print(dot_price)
-    
-    buy_dot = False
-    
-    if dot_price < 8.70:
-        buy_dot = True 
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        dot_data = response.json()
+        dot_price = round(float(dot_data["price"]), 2)
+        print(dot_price)
         
-    if buy_dot:
+        buy_dot = dot_price < 8.70
+        
         client = Client(TWILIO_SID, TWILIO_API)
         message = client.messages.create(
-            body="Buy some DOT ma boi!!!",
+            body="Buy some DOT ma boi!!!" if buy_dot else "HODL like vice grips!",
             from_="+18885998708",
             to="+16302012552"
         )
         print(message.status)
-    else:
-        client = Client(TWILIO_SID, TWILIO_API)
-        message = client.messages.create(
-            body="HODL like vice grips!",
-            from_="+18885998708",
-            to="+16302012552"
-        )
-        print(message.status)
+        
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
 
 if __name__ == "__main__":
     crypto_pair = "DOT-USD"  
